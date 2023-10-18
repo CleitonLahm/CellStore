8047470ed1be4150e8317bbb8431f58d1ce976fd
cleiton lahm <cleijllahm@gmail.com>
2023-10-18 10:18:53 -0300

    correcao de bug ao logar

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,29 +1,25 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { setItem } from "./services/LocalStorageFuncs";
-import { getItem } from "./services/LocalStorageFuncs";
+import { setItem, getItem } from "./services/LocalStorageFuncs";
 
 export const Login = (props) => {
-  const user = getItem('usuario')
-  const [name, setName] = useState(user?.name || "");
-  const [pass, setPass] = useState(user?.pass || "");
-  const [passIncorrect, setPassIncorrect] = useState(false)
+  const user = getItem('usuario');
+
+  // Se não houver um usuário no localStorage, crie um usuário padrão
+  if (!user) {
+    setItem('usuario', { name: '', pass: '' });
+  }
+
+  const [name, setName] = useState(user.name);
+  const [pass, setPass] = useState(user.pass);
+  const [passIncorrect, setPassIncorrect] = useState(false);
 
   const condition = name.length >= 3 && pass.length > 5;
 
   const saveUser = () => {
     // eslint-disable-next-line react/prop-types
     const { history: { push } } = props;
-    
-    if(name === user.name && pass === user.pass) {
-    push('/store')
-    return;
-    } else if (name === user.name && pass != user.pass) {
-      setPassIncorrect(true)
-      return;
-}
-    setItem('usuario', {name, pass})
-    push('/store')
+    setItem('usuario', { name, pass });
+    push('/store');
   }
 
   return (
@@ -42,17 +38,17 @@ export const Login = (props) => {
         value={pass}
       />
 
-        {
-          passIncorrect && <p>Password Incorrect</p>
-        }
+      {
+        passIncorrect && <p>Password Incorrect</p>
+      }
 
       <br></br>
       <br></br>
 
       <button 
-      type="button" 
-      disabled={!condition} 
-      onClick={() => saveUser(name, pass)}>
+        type="button" 
+        disabled={!condition} 
+        onClick={saveUser}>
         Sign in
       </button>
     </div>
